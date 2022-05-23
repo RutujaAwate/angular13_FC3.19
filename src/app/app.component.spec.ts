fbf0a146f4a153e98726bac3afbd13bcd2cdbd15
RutujaAwate <rutujaramchandra.awate@accolitedigital.com>
2022-05-23 23:19:19 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fusionchart13'`, () => {
+  it(`should have as title 'peer-dependency-angular-twelve'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fusionchart13');
+    expect(app.title).toEqual('peer-dependency-angular-twelve');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fusionchart13 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('peer-dependency-angular-twelve app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FusionChartsModule } from "angular-fusioncharts";
 
+// Load Maps  
+import * as Maps from 'fusioncharts/fusioncharts.maps';
+import * as Widgets from 'fusioncharts/fusioncharts.widgets';
+
+// Load WorldMap definition
+import * as World from 'fusioncharts/maps/fusioncharts.world';
+import * as TimeSeries from 'fusioncharts/fusioncharts.timeseries'; // Import timeseries
+
+// Import FusionCharts library and chart modules
+import * as FusionCharts from "fusioncharts";
+import * as charts from "fusioncharts/fusioncharts.charts";
+import * as FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import { Column2dComponent } from './column2d/column2d.component';
+import { SimplebarComponent } from './simplebar/simplebar.component';
+import { SimplelineComponent } from './simpleline/simpleline.component';
+import { Piechart2dComponent } from './piechart2d/piechart2d.component';
+import { SimplebubbleComponent } from './simplebubble/simplebubble.component';
+import { ScatterComponent } from './scatter/scatter.component';
+import { ParetoComponent } from './pareto/pareto.component';
+import { RatingmeterComponent } from './ratingmeter/ratingmeter.component';
+import { SimpleganttComponent } from './simplegantt/simplegantt.component';
+import { TreemapComponent } from './treemap/treemap.component';
+import { RadarComponent } from './radar/radar.component';
+import { MultipleseriestimeComponent } from './multipleseriestime/multipleseriestime.component';
+import { InteractivecandleComponent } from './interactivecandle/interactivecandle.component';
+import { ChordComponent } from './chord/chord.component';
+import { WorldmapComponent } from './worldmap/worldmap.component';
+
+// Pass the fusioncharts library and chart modules
+FusionChartsModule.fcRoot(FusionCharts, Maps, World,Widgets,TimeSeries, charts, FusionTheme);
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Column2dComponent, SimplebarComponent, SimplelineComponent, Piechart2dComponent, SimplebubbleComponent, ScatterComponent, ParetoComponent, RatingmeterComponent, SimpleganttComponent, TreemapComponent, RadarComponent, MultipleseriestimeComponent, InteractivecandleComponent, ChordComponent, WorldmapComponent],
+  imports: [BrowserModule, FusionChartsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
